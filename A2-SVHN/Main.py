01d810f9c6f27adf7ecc37631aa3f9a333316d2b
Tigger GL12CP(WINDOWS) <yixiu17@gmail.com>
2018-12-14 21:32:06 -0500

    Change the code to 2D Model and add comment

--- A2-SVHN/Main.py
+++ A2-SVHN/Main.py
@@ -9,7 +9,7 @@ Process:
 import scipy.io as sio
 import numpy as np
 import keras.utils as ku
-from A2 import get_model
+import A2 
 from timeit import default_timer as timer
 #from matplotlib.pyplot import imshow
 
@@ -20,19 +20,19 @@ from timeit import default_timer as timer
 train_mat = sio.loadmat("train_32x32.mat")
 test_mat = sio.loadmat("test_32x32.mat")
 
-# X values -- Training data, 'raw_data' stands for data with original shape
+### X values -- Training data, 'raw_data': data with original shape ###
 raw_train_x = train_mat['X']# (32, 32, 3, 73257)
 raw_test_x = test_mat['X']# (32, 32, 3, 26032)
 # Reshaping the X to roll the last value to the 1st 
 train_x = np.rollaxis(raw_train_x, 3)
 test_x = np.rollaxis(raw_test_x, 3)
+# This part of code is for 3D NN model
 # Reshaping the X to add 1 dimension at the end 
-train_x = np.expand_dims(train_x, axis = 4)
-test_x = np.expand_dims(test_x, axis = 4)
-#train_x = train_x[:, :, :, 0:1]
-#test_x = test_x[:, :, :, 0:1]
+#train_x = np.expand_dims(train_x, axis = 4)
+#test_x = np.expand_dims(test_x, axis = 4)
 
-# Y values -- Testing data, 'raw_data' stands for data with original shape
+
+### Y values -- Testing data, 'raw_data': data with original shape ###
 train_y = train_mat['y']# (73257, 1)
 test_y = test_mat['y']# (26032, 1)
 # Y values -- Reshape Y values to 1-of-K coding
@@ -44,7 +44,7 @@ test_y = ku.to_categorical(test_y, 10)# (73257, 10)
 
 ######################### Set up the NN model ################################
 
-model = get_model(20, 100)
+model = A2.get_model2D(20, 100)
 
 # Compile the NN model 
 model.compile(loss = "categorical_crossentropy", 
@@ -58,7 +58,7 @@ model.compile(loss = "categorical_crossentropy",
 start = timer()
 
 # Training Process
-fit = model.fit(train_x, train_y, epochs=50, batch_size=5000, verbose=2)
+fit = model.fit(train_x, train_y, epochs=60, batch_size=2000, verbose=2)
 
 # End Time
 end = timer()
